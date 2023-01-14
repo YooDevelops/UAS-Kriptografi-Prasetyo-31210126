da44480b724720e5f05aa6b302f2ce312bfc0116
Prasetyo <80664580+YooDevelops@users.noreply.github.com>
2023-01-14 14:56:56 +0700

    Add files via upload

---
+++ UAS_Kriptografi_Prasetyo_TI20C1/src/main.java
@@ -0,0 +1,86 @@
+import java.util.Scanner;
+
+public class main {
+    private static Scanner in;
+    public static void main(String[] args){
+        in = new Scanner(System.in);
+        System.out.println("Nama  : Prasetyo");
+        System.out.println("Kelas : TI 20 C 1");
+        System.out.println("NIM   : 312010126");
+        System.out.println("UAS - Kriptografi Semester Gasal 2022/2023");
+        System.out.println("");
+
+        System.out.print("1. Enkript\n2. Deskript\nPilih(1,2): ");
+        int choice = in.nextInt();
+        in.nextLine();
+
+        if(choice == 1){
+            System.out.println("---Enkript---");
+            cipherEncryption();
+        } else if (choice == 2){
+            System.out.println("---Deskript---");
+            cipherDecryption();
+        } else {
+            System.out.println("Pilihan yang anda masukkan salah");
+        }
+    }
+
+    private static void cipherDecryption() {
+        System.out.print("Masukan Pesan: ");
+        String msg = in.nextLine();
+
+        System.out.print("Masukkan kunci: ");
+        String key = in.nextLine();
+
+        String hexToDeci = "";
+        for (int i = 0; i < msg.length()-1; i+=2) {
+            // splitting hex into a pair of two
+            String output = msg.substring(i, (i+2));
+            int decimal = Integer.parseInt(output, 16);
+            hexToDeci += (char)decimal;
+        }
+
+        // decryption
+        String decrypText = "";
+        int keyItr = 0;
+        for (int i = 0; i < hexToDeci.length(); i++) {
+            // XOR Operation
+            int temp = hexToDeci.charAt(i) ^ key.charAt(keyItr);
+
+            decrypText += (char)temp;
+            keyItr++;
+            if(keyItr >= key.length()){
+                // once all of key's letters are used, repeat the key
+                keyItr = 0;
+            }
+
+        }
+
+        System.out.println("Pesan terdeskript: " + decrypText);
+    }
+
+    private static void cipherEncryption() {
+        System.out.print("Masukan Pesan: ");
+        String msg = in.nextLine();
+
+        System.out.print("Masukan Kunci: ");
+        String key = in.nextLine();
+
+        String encrypHexa = "";
+        int keyItr = 0;
+        for (int i = 0; i < msg.length(); i++) {
+            // XOR Operation
+            int temp = msg.charAt(i) ^ key.charAt(keyItr);
+
+            encrypHexa += String.format("%02x", (byte)temp);
+            keyItr++;
+            if(keyItr >= key.length()){
+                // once all of key's letters are used, repeat the key
+                keyItr = 0;
+            }
+
+        }
+
+        System.out.println("Pesan terenkript: " + encrypHexa);
+    }
+}
